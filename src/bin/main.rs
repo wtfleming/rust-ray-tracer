c660e5e96fef4730619815b60bce3955830711db
Will Fleming <wfleming77@gmail.com>
2019-12-31 18:25:39 -0500

    add scene with a circle

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,15 +1,58 @@
 use std::fs;
+use std::rc::Rc;
 
 use rust_ray_tracer::canvas;
 use rust_ray_tracer::color;
 use rust_ray_tracer::mathf;
 use rust_ray_tracer::mathf::matrix;
+use rust_ray_tracer::mathf::ray;
+use rust_ray_tracer::mathf::sphere;
 use rust_ray_tracer::mathf::vector3;
 use rust_ray_tracer::ppm;
+use rust_ray_tracer::mathf::intersection;
+
 
 fn main() {
     // draw_simple();
-    draw_clock();
+    // draw_clock();
+    draw_circle();
+}
+
+fn draw_circle() {
+    let wall_z = 10.0;
+    let wall_size = 7.0;
+    let canvas_pixels = 100;
+    let pixel_size = wall_size / (canvas_pixels as f64);
+    let half = wall_size / 2.0;
+
+    let mut canvas = canvas::new(canvas_pixels, canvas_pixels);
+    let color = color::red();
+
+
+    let shape = Rc::new(sphere::new());
+
+    for y in 0..canvas_pixels {
+        let world_y = half - pixel_size * (y as f64);
+        for x in 0..canvas_pixels {
+            let world_x = -half + pixel_size * (x as f64);
+
+            let position = vector3::new(world_x, world_y, wall_z);
+            let ray_origin = vector3::new(0.0, 0.0, -5.0);
+            let ray_origin2 = vector3::new(0.0, 0.0, -5.0);
+            let r = ray::new(ray_origin, position.subtract(&ray_origin2));
+            let xs = r.intersect(Rc::clone(&shape));
+
+            let xs = intersection::new_intersections(xs);
+            let hit = ray::hit(&xs);
+            if hit.is_some() {
+                canvas.write_pixel(x as isize, y as isize, &color);
+            }
+        }
+    }
+
+    let ppm_data = ppm::canvas_to_ppm(&canvas);
+    fs::write("renders/circle.ppm", ppm_data).expect("Unable to write file");
+
 }
 
 #[allow(dead_code)]
@@ -26,6 +69,7 @@ fn draw_simple() {
     fs::write("renders/output.ppm", ppm_data).expect("Unable to write file");
 }
 
+#[allow(dead_code)]
 fn draw_clock() {
     let mut canvas = canvas::new(100, 100);
     let radius = 30.0;
